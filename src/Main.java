4936d0a2de1b2c05b9e46ab1f2ffa33daa24d81a
Brian Simpson <bsimps3@gmail.com>
2020-01-27 15:05:27 -0600

    added a thing

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println( Calculator.add(5,8));
         System.out.println( Calculator.divide(10, 3));
+        System.out.println( "hello");
     }
 
 }
