5fbc65c7fa2745e2a95990cabf9a249dbab363fd
Brian Simpson <bsimps3@gmail.com>
2020-01-27 14:54:58 -0600

    initial commit

---
+++ src/Calculator.java
@@ -0,0 +1,15 @@
+
+public class Calculator {
+
+    public static int add(int in1, int in2) {
+        return in1+in2;
+    }
+    
+    public static int multiply(int in1, int in2) {
+        return in1*in2;
+    }
+    
+    public static int divide(int in1, int in2) {
+        return in1/in2;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,9 @@
+
+public class Main {
+    
+    public static void main(String[] args) {
+        System.out.println( Calculator.add(5,8));
+        System.out.println( Calculator.divide(10, 3));
+    }
+
+}
